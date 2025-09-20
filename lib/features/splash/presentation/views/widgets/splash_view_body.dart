8390e963ca8144b8b716051c2740e5e65f68d54f
Ehab Mostafa <hobamostafa205@gmail.com>
2025-09-20 16:19:52 +0300

    adding fading animation to splash view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,11 +1,45 @@
 import 'package:curely/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    animation = Tween<double>(begin: 0, end: 1).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Image.asset(AssetsData.splashLogo));
+    return Center(
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) => FadeTransition(
+          opacity: animation,
+          child: Image.asset(AssetsData.splashLogo),
+        ),
+      ),
+    );
   }
 }
